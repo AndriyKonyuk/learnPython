10e8e11e5977688581021870f8ec21367a3b9a2b
AndriyKonyuk <andriykonyuk@gmail.com>
2017-03-14 16:34:03 +0200

    First task, function like().

---
+++ lect_1_task.py
@@ -0,0 +1,19 @@
+def like(numbers: str, a_set: str, b_set: str):
+    likes = 0
+    numbers, a_set, b_set = numbers.split(' '), a_set.split(' '), b_set.split(' ')
+    for i in numbers:
+        if i in a_set and i not in b_set:
+            likes += 1
+        elif i in b_set and i not in a_set:
+            likes -= 1
+    return likes
+
+numbers = '3 2 10 7 5 5 2 1 2'
+a = '2 3 7'
+b = '5 10 7'
+print(like(numbers, a, b))
+
+numbers = '1 4 10 20 1 11 12'
+a = '1 4 1 12'
+b = '1 12 10 20'
+print(like(numbers, a, b))
