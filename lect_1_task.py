e5a9edb0979696d4cfcf06bebcad1afe7888fd3c
AndriyKonyuk <andriykonyuk@gmail.com>
2017-03-17 13:31:35 +0200

    All errors corrected.

--- lect_1_task.py
+++ lect_1_task.py
@@ -1,34 +1,34 @@
-import string
-
-
-def like(numbers: str, a_set: str, b_set: str):
-    likes = 0
-    numbers, a_set, b_set = numbers.split(' '), a_set.split(' '), b_set.split(' ')
-    for i in numbers:
-        if i in a_set and i not in b_set:
-            likes += 1
-        elif i in b_set and i not in a_set:
-            likes -= 1
-    return likes
-
-
-numbers = '3 2 10 7 5 5 2 1 2'
-a = '2 3 7'
-b = '5 10 7'
-print(like(numbers, a, b))
-
-numbers = '1 4 10 20 1 11 12'
-a = '1 4 1 12'
-b = '1 12 10 20'
-print(like(numbers, a, b))
-
-
-def fine_print(n: int):
-    for i in range(n + 1):
-        print('%-10d %-10o %-10x %-10s' % (i, i, i, bin(i)[2:]))
-
-
-fine_print(10)
+# import string
+#
+#
+# def like(numbers: str, a_set: str, b_set: str):
+#     likes = 0
+#     numbers, a_set, b_set = numbers.split(' '), a_set.split(' '), b_set.split(' ')
+#     for i in numbers:
+#         if i in a_set and i not in b_set:
+#             likes += 1
+#         elif i in b_set and i not in a_set:
+#             likes -= 1
+#     return likes
+#
+#
+# numbers = '3 2 10 7 5 5 2 1 2'
+# a = '2 3 7'
+# b = '5 10 7'
+# print(like(numbers, a, b))
+#
+# numbers = '1 4 10 20 1 11 12'
+# a = '1 4 1 12'
+# b = '1 12 10 20'
+# print(like(numbers, a, b))
+#
+#
+# def fine_print(n: int):
+#     for i in range(n + 1):
+#         print('%-10d %-10o %-10x %-10s' % (i, i, i, bin(i)[2:]))
+#
+#
+# fine_print(10)
 
 
 def decorator(f):
@@ -40,7 +40,6 @@ def decorator(f):
             print('Input args:', args)
             print('Input kwargs:', kwargs)
             return None
-
     return wrapper
 
 @decorator
@@ -48,23 +47,23 @@ def func(x, y):
     return x / y
 
 
-func(10, 2, op='division', base=10)
+print(func(10, 2, 7,8,9, op='division', base=10))
 
 
-def filter_func(list_email: list):
-    def fil_fun(email):
-        if '@' in email and len(email.split('@')) == 2 and email[0] in string.ascii_letters or '_' in email[0] and len(
-                email.split('@')[1].split('.')) > 1:
-            for i in email.split('@')[0]:
-                if (i not in string.ascii_letters) and (i not in string.digits) and (i not in '_'):
-                    return None
-            for i in email.split('@')[1].split('.'):
-                if not len(i) > 1:
-                    return None
-            return email
-
-    return list(filter(fil_fun, list_email))
-
-
-emails = ['abc@gmail.com.ua', '*@ank.com', '_ny@us.gov.us', 'z@b.kk', 'a@co.com', 'abc@c.c']
-print(filter_func(emails))
+# def filter_func(list_email: list):
+#     def fil_fun(email):
+#         if '@' in email and len(email.split('@')) == 2 and email[0] in string.ascii_letters or '_' in email[0] and len(
+#                 email.split('@')[1].split('.')) > 1:
+#             for i in email.split('@')[0]:
+#                 if (i not in string.ascii_letters) and (i not in string.digits) and (i not in '_'):
+#                     return None
+#             for i in email.split('@')[1].split('.'):
+#                 if not len(i) > 1:
+#                     return None
+#             return email
+#
+#     return list(filter(fil_fun, list_email))
+#
+#
+# emails = ['abc@gmail.com.ua', '*@ank.com', '_ny@us.gov.us', 'z@b.kk', 'a@co.com', 'abc@c.c']
+# print(filter_func(emails))
