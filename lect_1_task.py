c9a78b9c9f190edd5b1f64e461967898744c9cf2
AndriyKonyuk <andriykonyuk@gmail.com>
2017-03-17 11:54:03 +0200

    All errors corrected.

--- lect_1_task.py
+++ lect_1_task.py
@@ -1,5 +1,6 @@
 import string
 
+
 def like(numbers: str, a_set: str, b_set: str):
     likes = 0
     numbers, a_set, b_set = numbers.split(' '), a_set.split(' '), b_set.split(' ')
@@ -9,6 +10,7 @@ def like(numbers: str, a_set: str, b_set: str):
         elif i in b_set and i not in a_set:
             likes -= 1
     return likes
+
 
 numbers = '3 2 10 7 5 5 2 1 2'
 a = '2 3 7'
@@ -22,39 +24,47 @@ print(like(numbers, a, b))
 
 
 def fine_print(n: int):
-    for i in range(n+1):
-        print('%-10d %-10o %-10x %-10s' % (i,i,i,bin(i)))
+    for i in range(n + 1):
+        print('%-10d %-10o %-10x %-10s' % (i, i, i, bin(i)[2:]))
 
-fine_print(1000)
 
-def decorator(fun:object):
-    def wrapper(x, y, **kwargs):
+fine_print(10)
+
+
+def decorator(f):
+    def wrapper(*args, **kwargs):
         try:
-            return fun(x,y, **kwargs)
+            return f(args[0], args[1])
         except Exception as err:
-            print('Exception occurred in func:', fun.__name__, err)
-            print('Input args:', x, y)
+            print('Exception occurred in func:', f.__name__, err)
+            print('Input args:', args)
             print('Input kwargs:', kwargs)
             return None
+
     return wrapper
 
-
 @decorator
-def func(x, y, **kwargs):
+def func(x, y):
     return x / y
 
-print(func(10,0,op='division',base=20))
+
+func(10, 2, op='division', base=10)
+
 
 def filter_func(list_email: list):
     def fil_fun(email):
-        if '@' in email and len(email.split('@')) == 2 and len(email.split('@')[0]) > 1 and email[
-            0] in string.ascii_letters or '_' in email[0] and len(email.split('@')[1].split('.')) > 1:
+        if '@' in email and len(email.split('@')) == 2 and email[0] in string.ascii_letters or '_' in email[0] and len(
+                email.split('@')[1].split('.')) > 1:
             for i in email.split('@')[0]:
                 if (i not in string.ascii_letters) and (i not in string.digits) and (i not in '_'):
+                    return None
+            for i in email.split('@')[1].split('.'):
+                if not len(i) > 1:
                     return None
             return email
 
     return list(filter(fil_fun, list_email))
 
-emails = ['abc@gmail.com.ua', '*@ank.com', '_ny@us.gov.us', 'z@b.kk']
+
+emails = ['abc@gmail.com.ua', '*@ank.com', '_ny@us.gov.us', 'z@b.kk', 'a@co.com', 'abc@c.c']
 print(filter_func(emails))
