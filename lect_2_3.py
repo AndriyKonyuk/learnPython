475edbaba996aedef3d45ba70522b2640b793d47
Andrii <andriykonyuk@gmail.com>
2017-03-23 00:32:46 +0200

    Create class AOpen(), add methods write(), close().

--- lect_2_3.py
+++ lect_2_3.py
@@ -8,29 +8,37 @@ class AOpen:
         self.encoding = encoding
 
         mode_list = [('r', os.O_RDONLY), ('w', os.O_WRONLY | os.O_CREAT), ('x', os.O_WRONLY | os.O_EXCL),
-                     ('a', os.O_APPEND), ('+', os.O_RDWR)]  # mode 't' and 'b' not exist in module os
+                     ('a', os.O_APPEND | os.O_CREAT), ('+', os.O_RDWR | os.O_CREAT)]  # mode 't' and 'b' not exist in module os
         mode_dict = {key: value for key, value in mode_list}
         if self.mode in mode_dict:
             self.mode = mode_dict[self.mode]
         self.open_file = os.open(self.filename, self.mode)
 
     def read(self):
-        return os.read(self.open_file, os.stat(self.open_file).st_size).decode('UTF-8')
-        # (os.read(y, os.stat(y).st_size)).decode('UTF-8')
+        os.lseek(self.open_file, 0, os.SEEK_SET)
+        return os.read(self.open_file, os.stat(self.open_file).st_size).decode('utf-8')
 
     def readLine(self):
         pass
 
-    def write(self, s):
-        pass
+    def write(self, s:str):
+        if self.mode == os.O_WRONLY | os.O_CREAT:
+            os.ftruncate(self.open_file, 0)
+            os.lseek(self.open_file, 0, os.SEEK_SET)
+            os.write(self.open_file, s.encode())
+
+        else:
+            os.lseek(self.open_file, os.stat(self.open_file).st_size, os.SEEK_SET)
+            os.write(self.open_file, s.encode())
 
     def writeLine(self, s):
         pass
 
     def close(self):
-        pass
+        os.close(self.open_file)
 
 
 filepath = 'file.txt'
-x = AOpen(filepath)
+x = AOpen(filepath, 'r')
+x.write("hello world \n this is test")
 print(x.read())
