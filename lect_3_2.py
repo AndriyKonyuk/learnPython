98a9e64b5f4749aa3912de687d3ef4ff2a2522ef
Andrii <andriykonyuk@gmail.com>
2017-03-30 12:06:37 +0300

    Second task, problem with json parse.

--- lect_3_2.py
+++ lect_3_2.py
@@ -1,37 +1,33 @@
 from abc import ABC, abstractmethod
-import csv
-import json
-
+import csv, json
 
 def csv_load(file: object) -> str:
-    s = csv.reader(file, delimiter=' ', quotechar='|')
+    s = csv.reader(file, delimiter='\n')
     return s
 
-
 def csv_save(s: str, file: object) -> None:
-    spamwriter = csv.writer(file, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter = csv.writer(file, delimiter=' ', quotechar='\n', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(s)
 
-
 def json_load(file: object) -> str:
-    res = json.loads(file)
+    x = file.read().replace('\'', '\"')
+    res = json.loads(x)
     return res
 # http://stackoverflow.com/questions/18514910/how-do-i-automatically-fix-an-invalid-json-string
 
 def json_save(s: str, file: object) -> None:
-    while True:
-        try:
-            json.dump(s, file)
-            break
-        except Exception as a:
-            print(a)
+    s_n = {'rows':[]}
+    if not isinstance(s, str):
+        for i in s:
+            s_n["rows"].append(i[0])
+        json.dump(s_n, file)
+    else:
+        json.dump(s_n["rows"].append(s), file)
 
 class AbsConverterFabric(ABC):
     @abstractmethod
     def create_converter(self, _from: str, _to: str) -> object:
         raise NotImplemented
-
-
 class AbstractConverter(ABC):
     @abstractmethod
     def load(self, file: object) -> str:
@@ -40,7 +36,6 @@ class AbstractConverter(ABC):
     @abstractmethod
     def save(self, s: str, file: object) -> object:
         raise NotImplemented
-
 
 class ConverterFabric(AbsConverterFabric):
     def create_converter(self, _from: str, _to: str) -> object:
@@ -65,21 +60,23 @@ class ConverterFabric(AbsConverterFabric):
                     print('Function name _to isn\'t corect')
 
         return Converter()
-f = open('test.txt', 'w')
+
 fab = ConverterFabric()
-
 converter1 = fab.create_converter('csv', 'json')
-
 converter2 = fab.create_converter('json', 'csv')
 
-string = 'Joe Doe Green 77'
+x =  open('csv.txt', 'r')
+result = converter1.load(x)
+print(result)
 
-with open('json.txt', 'w') as file:
-    converter1.save(string, file)
+print()
+y = open('json.txt', 'w')
+converter1.save(result, y)
 
-with open('json.txt', 'r') as file:
-    result = converter2.load(file)
-    print(result)
+z = open('json.txt', 'r')
+result = converter2.load(z)
 
-with open('csv.txt', 'w') as file:
-    converter2.save(result, file)
+print(result)
+
+t = open('csv.txt', 'w')
+converter2.save(result, t)
